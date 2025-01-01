62d33ed23280af46e539e678ec2db13f7ef7552c
Snehith007 <msnehith007@gmail.com>
2025-01-01 16:48:59 +0530

    ..

---
+++ laptops.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin >> n;
+    int a, b;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        if (a > b)
+        {
+            cout << "Happy Alex";
+            return 0;
+        }
+    }
+    cout << "Poor Alex";
+    return 0;
+}
